f337f32b8f12919c60a519b9ca1209ba6a95acab
narij <narij@DESKTOP-KH2FN7U>
2020-02-17 22:13:06 +0900

    2020 충돌..?

--- src/main/java/com/lec/mbg/command/MyPageInfoCommand.java
+++ src/main/java/com/lec/mbg/command/MyPageInfoCommand.java
@@ -2,11 +2,14 @@ package com.lec.mbg.command;
 
 import org.springframework.ui.Model;
 
+import com.lec.mbg.mybatis.beans.MyPageInfoDTO;
+
 public class MyPageInfoCommand implements Command {
 
 	@Override
 	public void execute(Model model) {
 		
+		MyPageInfoDTO dto;
 		
 	}
 
